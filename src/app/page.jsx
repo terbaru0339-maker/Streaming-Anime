589ebdc6c6bfe83003e251e15f291e1f1350010c
terbaru0339-maker <terbaru0339@gmail.com>
2025-11-21 11:52:38 +0700

    Update page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,1 +1,16 @@
+import Image from "next/image";
 
+const API_URL = 'https://www.sankavollerei.com/anime/'
+
+const getAnime = async () => {
+  const response = await fetch (API_URL)
+  const data = await response.json()
+  return data.ongoing
+}
+
+export default async function home () {
+  const anime = getAnime()
+
+  return (
+    
+  
